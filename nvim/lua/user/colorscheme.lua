3414505837c77cc513a30c376ff39b51ca9fb0e4
PJ Dietz <pjdietz@gmail.com>
2022-07-06 16:51:29 -0400

    Tweak style for comments

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -3,5 +3,16 @@ if not status_ok then
 	return
 end
 
+local colors = require('material.colors')
+
+material.setup({
+  custom_highlights = {
+    Comment = {
+      fg = colors.text,
+      italic = true
+    }
+  }
+})
+
 vim.g.material_style = 'darker'
 vim.cmd 'colorscheme material'
