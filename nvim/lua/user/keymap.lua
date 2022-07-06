1950bb3e495f019c09b38c9431fd1cc18925ab68
PJ Dietz <pjdietz@gmail.com>
2022-07-06 16:51:51 -0400

    Add nohl keymap

--- nvim/lua/user/keymap.lua
+++ nvim/lua/user/keymap.lua
@@ -2,33 +2,36 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 local map = vim.keymap.set
-        
+
 -- Y works like C and D
 map('n', 'Y', 'y$')
 
 -- Splits
-map('n', '<leader>|', '<C-w>v')
-map('n', '<leader>-', '<C-w>s')
+map('n', '<Leader>|', '<C-w>v')
+map('n', '<Leader>-', '<C-w>s')
 map('n', '<C-h>', '<C-w>h')
 map('n', '<C-j>', '<C-w>j')
 map('n', '<C-k>', '<C-w>k')
 map('n', '<C-l>', '<C-w>l')
 
 -- Buffers
-map('n', '<s-tab>', '<cmd>bn<cr>')
-map('n', '<leader><tab>', '<cmd>bp|bd #<cr>')
-map('n', '<leader>ww', '<cmd>bufdo bwideout<cr>')
+map('n', '<S-tab>', '<CMD>bn<CR>')
+map('n', '<Leader><Tab>', '<CMD>bp|bd #<CR>')
+map('n', '<Leader>ww', '<CMD>bufdo bwideout<CR>')
 
 -- Toggle invisibles
-map('n', '<leader>i', ':set list!<cr>')
+map('n', '<Leader>i', '<CMD>set list!<CR>')
+
+-- Clean highlight
+map('n', '<Leader><Esc>', '<CMD>nohl<CR>')
 
 -- Git and merge
-map('n', '<leader>gh', ':diffget //2<CR>')
-map('n', '<leader>gl', ':diffget //3<CR>')
+map('n', '<Leader>gh', '<CMD>diffget //2<CR>')
+map('n', '<Leader>gl', '<CMD>diffget //3<CR>')
 
 -- Move lines up or down
-map('x', 'K', ":move '<-2<CR>gv-gv")
-map('x', 'J', ":move '>+1<CR>gv-gv")
+map('x', 'K', "<CMD>move '<-2<CR>gv-gv")
+map('x', 'J', "<CMD>move '>+1<CR>gv-gv")
 
 -- Indent and keep selection
 map({'v', 'x'}, '<', '<gv')
