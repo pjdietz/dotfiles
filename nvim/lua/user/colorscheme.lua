42625dac5b428189878b89d864fd3c953db5b86f
PJ Dietz <pjdietz@gmail.com>
2022-07-09 09:15:22 -0400

    Tweaks to colorscheme

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -3,21 +3,25 @@ local colors = require "material.colors"
 
 colors.bg_alt2 = "#2C2C2C"
 colors.subtle = "#94A7B0"
+colors.darkorange = "#E2795B"
 
-material.setup({
+material.setup {
   custom_highlights = {
     Comment = { fg = colors.text, italic = true },
     ColorColumn = { bg = colors.bg_alt2 },
     CursorLine = { bg = colors.bg_alt2 },
+    TSAttribute = { fg = colors.green },
+    TSInclude = { fg = colors.darkblue },
     TSKeyword = { fg = colors.purple },
+    TSKeywordReturn = { fg = colors.purple },
     TSMethod = { fg = colors.blue },
     TSProperty = { fg = colors.fg },
-    TSType = { fg = colors.yellow },
+    TSType = { fg = colors.white },
     TSVariable = { fg = colors.orange },
-    TSVariableBuiltin = { fg = colors.fg },
-    UserDollar = { fg = colors.subtle },
+    TSVariableBuiltin = { fg = colors.white },
+    UserDollar = { fg = colors.fg },
   }
-})
+}
 
 vim.g.material_style = "darker"
 vim.cmd "colorscheme material"
