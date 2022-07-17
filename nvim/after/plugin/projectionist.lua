d49717766671df6b09e1fe6414c7acff20e92259
PJ Dietz <pjdietz@gmail.com>
2022-07-17 15:09:06 -0400

    Add projectionist

---
+++ nvim/after/plugin/projectionist.lua
@@ -0,0 +1,4 @@
+-- Switch to alternative file.
+vim.keymap.set("n", "<Leader>aa", "<CMD>A<CR>")
+-- Switch to alternative file in vertical split.
+vim.keymap.set("n", "<Leader>av", "<CMD>AV<CR>")
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -109,6 +109,7 @@ return packer.startup(function (use)
   use "tpope/vim-commentary"
   use "tpope/vim-fugitive"
   use "tpope/vim-obsession"
+  use "tpope/vim-projectionist"
   use "tpope/vim-surround"
   use "vim-test/vim-test"
   use "vimwiki/vimwiki"
