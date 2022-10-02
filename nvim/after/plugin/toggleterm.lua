76e054c6f18186b1e8435f34312d8664c6819aba
PJ Dietz <pjdietz@gmail.com>
2022-10-02 17:19:33 -0400

    Toggleterm opens floating window

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -1,6 +1,9 @@
 require("toggleterm").setup {
   open_mapping = [[<F1>]],
-  direction = "horizontal",
+  direction = "float",
+  float_opts = {
+    border = "curved"
+  },
   size = 20,
   close_on_exit = true,
   hide_numbers = true,
