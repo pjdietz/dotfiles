4658f8436d7e0fc70fa6968a0e09c833483969a7
PJ Dietz <pjdietz@gmail.com>
2022-11-18 15:15:31 -0500

    Use icons for diagnositcs

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -32,6 +32,18 @@ local on_attach = function (_, bufnr)
 
 end
 
+local signs = {
+    Error = " ",
+    Warn = " ",
+    Hint = " ",
+    Info = " "
+}
+
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
+end
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.foldingRange = {
     dynamicRegistration = false,
