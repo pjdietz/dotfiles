a5772457785197cb17b52dc78229e4da80bc4959
PJ Dietz <pjdietz@gmail.com>
2023-01-22 18:57:40 -0500

    Automcd runs PHP-CS-Fixer only when config file is present in project

--- nvim/.config/nvim/after/ftplugin/php.lua
+++ nvim/.config/nvim/after/ftplugin/php.lua
@@ -3,8 +3,20 @@ vim.opt_local.commentstring = '// %s'
 
 local group = vim.api.nvim_create_augroup("USER-PHP", { clear = true })
 
+local function config_file_present()
+   return (vim.fn.filereadable(".php-cs-fixer.dist.php") == 1) or
+      (vim.fn.filereadable(".php-cs-fixer.php") == 1)
+end
+
+local function fix_cs_cmd(opts)
+   if config_file_present() then
+      vim.cmd([[silent! !php-cs-fixer fix ]] .. opts.file)
+   end
+end
+
 vim.api.nvim_create_autocmd("BufWritePost", {
    desc = "Run php-cs-fixer after saving",
-   command = "silent! !php-cs-fixer fix <afile>",
+   callback = fix_cs_cmd,
+   buffer = 0,
    group = group
 })
