49219eddfb71d6ac6c833838bc02020bade54949
PJ Dietz <pjdietz@gmail.com>
2023-01-31 16:25:52 -0500

    Disable mini.animate

--- nvim/.config/nvim/after/plugin/mini.lua
+++ nvim/.config/nvim/after/plugin/mini.lua
@@ -1,13 +1,14 @@
 local animate = require('mini.animate')
 animate.setup {
   cursor = {
-    timing = animate.gen_timing.linear({ duration = 150, unit = 'total' }),
+    enable = false,
+    timing = animate.gen_timing.linear({ duration = 125, unit = 'total' }),
     path = animate.gen_path.line({
       predicate = function() return true end,
     }),
   },
   scroll = {
-    enable = true,
+    enable = false,
     timing = animate.gen_timing.linear({ duration = 150, unit = 'total' }),
     subscroll = animate.gen_subscroll.equal({ max_output_steps = 120 }),
   }
