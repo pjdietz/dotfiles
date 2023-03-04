19e0ecdd837838bc583794b712569440a155cbe1
PJ Dietz <pjdietz@gmail.com>
2023-03-04 15:04:41 -0500

    Tmux module checks if Tmux is running and active

--- nvim/.config/nvim/lua/user/tmux.lua
+++ nvim/.config/nvim/lua/user/tmux.lua
@@ -1,10 +1,21 @@
 local M = {}
 
+function M.active()
+  return os.getenv("TMUX") and M.running()
+end
+
+function M.running()
+  vim.fn.system([[pgrep tmux]])
+  return vim.v.shell_error == 0
+end
+
 function M.show_status()
+  if not M.active() then return end
   vim.fn.system([[tmux set status on]])
 end
 
 function M.hide_status()
+  if not M.active() then return end
   vim.fn.system([[tmux set status off]])
 end
 
@@ -14,12 +25,14 @@ function M.zoom()
 end
 
 function M.is_zoomed()
+  if not M.active() then return end
   -- Check for the zoom flag
   vim.fn.system([[tmux list-panes -F '#F' | grep -q Z]])
   return vim.v.shell_error == 0
 end
 
 function M.get_session_name()
+  if not M.active() then return "" end
   local name = vim.fn.system([[tmux display-message -p '#S']])
   return (name:gsub("^%s*(.-)%s*$", "%1"))
 end
