281ddaf71c44c5e9c6c9b684bb7bc8f97fed5508
PJ Dietz <pjdietz@gmail.com>
2023-07-14 08:35:27 -0400

    Add keybind for vimwiki checkbox toggle <Leader>x

--- nvim/.config/nvim/lua/plugins/vimwiki.lua
+++ nvim/.config/nvim/lua/plugins/vimwiki.lua
@@ -2,6 +2,7 @@ return {
   "vimwiki/vimwiki",
   keys = {
     { "<Leader>ww", "<Plug>VimwikiIndex", { desc = "Open Vimwiki" } },
+    { "<Leader>x", "<Plug>VimwikiToggleListItem", { desc = "Toggle List Item" } },
   },
   ft = { "markdown", "vimwiki" },
   init = function()
