208c268dbbfd271445a8acfb8534555c1e24c0ba
PJ Dietz <pjdietz@gmail.com>
2023-09-18 13:01:19 -0400

    Configure Dad Bod UI

--- nvim/.config/nvim/lua/plugins/database.lua
+++ nvim/.config/nvim/lua/plugins/database.lua
@@ -1,19 +1,28 @@
 return {
-  'kristijanhusak/vim-dadbod-ui',
+  "kristijanhusak/vim-dadbod-ui",
   dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
   },
   cmd = {
-    'DBUI',
-    'DBUIToggle',
-    'DBUIAddConnection',
-    'DBUIFindBuffer',
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
   },
   init = function()
     -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_save_location = "~/projects/arc/db"
+
+    vim.api.nvim_set_hl(0, "NotificationInfo", { bg="#1e1e2e", fg="#a6e3a1" })
+    vim.api.nvim_set_hl(0, "NotificationWarning", { bg="#1e1e2e", fg="#f9e2af" })
+    vim.api.nvim_set_hl(0, "NotificationError", { bg="#1e1e2e", fg="#f38ba8" })
+
   end,
+  keys = {
+    { "<Leader>db", "<CMD>DBUIToggle<CR>", { desc = "[D]BUI Toggle" } }
+  },
   config = function()
     require("user.dadbod").setup()
   end,
--- nvim/.config/nvim/lua/user/dadbod.lua
+++ nvim/.config/nvim/lua/user/dadbod.lua
@@ -5,8 +5,6 @@ local function db_completion()
 end
 
 function M.setup()
-  vim.g.db_ui_save_location = vim.fn.stdpath "config" .. require("plenary.path").path.sep .. "db_ui"
-
   vim.api.nvim_create_autocmd("FileType", {
     pattern = {
       "sql",
