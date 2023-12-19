9b1e28f6ace2f4b7720e0bd75f952b0b8238ab8b
PJ Dietz <pjdietz@gmail.com>
2023-12-19 08:58:55 -0500

    C# icon

---
+++ nvim/.config/nvim/lua/plugins/icons.lua
@@ -0,0 +1,13 @@
+return {
+  "kyazdani42/nvim-web-devicons",
+  opts = {
+    override = {
+      cs = {
+        icon = "",
+        color = "#428850",
+        cterm_color = "65",
+        name = "CSharp"
+      }
+    }
+  }
+}
