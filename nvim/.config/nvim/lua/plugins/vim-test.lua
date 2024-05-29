74f4144de49d78adf03340a58f0c4320f5b06eca
PJ Dietz <pjdietz@gmail.com>
2024-05-29 06:39:34 -0400

    Upgrade to Neovim 0.10

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -1,11 +1,11 @@
 return {
   "vim-test/vim-test",
   keys = {
-    { "<Leader>tn", "<CMD>TestNearest<CR>", desc = { "[T]est [N]earest" } },
-    { "<Leader>tf", "<CMD>TestFile<CR>", desc = { "[T]est [F]ile" } },
-    { "<Leader>ts", "<CMD>TestSuite<CR>", desc = { "[T]est [S]uite" } },
-    { "<Leader>tl", "<CMD>TestLast<CR>", desc = { "[T]est [L]ast" } },
-    { "<Leader>tv", "<CMD>TestVisit<CR>", desc = { "[T]est [V]isit" } },
+    { "<Leader>tn", "<CMD>TestNearest<CR>", desc = "[T]est [N]earest" },
+    { "<Leader>tf", "<CMD>TestFile<CR>", desc = "[T]est [F]ile" },
+    { "<Leader>ts", "<CMD>TestSuite<CR>", desc = "[T]est [S]uite" },
+    { "<Leader>tl", "<CMD>TestLast<CR>", desc = "[T]est [L]ast" },
+    { "<Leader>tv", "<CMD>TestVisit<CR>", desc = "[T]est [V]isit" },
   },
   init = function()
     vim.cmd [[
