cb893aa13a38a34f8b5fd47cbe0130d7e0b5a83d
PJ Dietz <pjdietz@gmail.com>
2024-07-02 15:29:44 -0400

    Configure telescope; move options

--- nvim-new/.config/nvim-new/init.lua
+++ nvim-new/.config/nvim-new/init.lua
@@ -1,19 +1,2 @@
-require("user.options")
-
-
--- Lazy Plugin Manager
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-
-  checker = { enabled = true },
-})
+require("config.options")
+require("config.lazy")
