37b164fd86d35363c8afbec5fe96e8cc65c73feb
PJ Dietz <pjdietz@gmail.com>
2024-09-13 18:22:50 -0400

    Markdown changes to todo lists, headers, lists

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,2 +1,3 @@
 vim.wo.wrap = true
 vim.wo.linebreak = true
+vim.opt.colorcolumn = {}
--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -4,13 +4,26 @@ return {
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
   config = function ()
     require("render-markdown").setup({
+      heading = {
+        position = "inline",
+        icons = { "󰉫", "󰉬", "󰉭", "󰉮", "󰉯", "󰉰" },
+        signs = { "󰌕" },
+      },
+      bullet = {
+        icons = { "", "", "", "" },
+      },
       checkbox =  {
         unchecked = {
           icon = "󰄱",
         },
         checked = {
           icon = "󰱒"
-        }
+        },
+        custom = {
+          progress1 = { raw = "[.]", rendered = '', highlight = 'RenderMarkdownUnchecked' },
+          progress2 = { raw = "[o]", rendered = '', highlight = 'RenderMarkdownUnchecked' },
+          progress3 = { raw = "[O]", rendered = '', highlight = 'RenderMarkdownUnchecked' },
+        },
       }
     })
   end
