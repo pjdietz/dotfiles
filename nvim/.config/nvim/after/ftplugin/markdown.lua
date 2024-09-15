fc2d21ea4d20bcb337c87ead1612e64b01586840
PJ Dietz <pjdietz@gmail.com>
2024-09-15 19:37:14 -0400

    Add keybinds for navigating markdown headers

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,6 @@
 vim.wo.wrap = true
 vim.wo.linebreak = true
 vim.opt.colorcolumn = {}
+
+vim.keymap.set("n", "<C-j>", [[/^##\+ .*<CR>:nohlsearch<CR>]], { desc = "Next Header", buffer = true, silent = true })
+vim.keymap.set("n", "<C-k>", [[?^##\+ .*<CR>:nohlsearch<CR>]], { desc = "Previous Header", buffer = true, silent = true })
