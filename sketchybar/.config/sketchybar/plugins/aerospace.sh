c03d1c13eebae7e616e4a6b1f0081c5916a3bb8e
PJ Dietz <pjdietz@gmail.com>
2024-11-28 10:17:07 -0500

    show only workspaces with windows in sketchybar

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -2,7 +2,13 @@
 
 FOCUSED_WORKSPACE="${FOCUSED_WORKSPACE:-$(aerospace list-workspaces --focused)}"
 
-echo "1: $1, FW: $FOCUSED_WORKSPACE, N: $NAME" >> ~/log.txt
+WINDOWS=$(aerospace list-windows --workspace "$1")
+
+if [ -n "${WINDOWS}" ]; then
+  sketchybar --set "${NAME}" drawing=on
+else
+  sketchybar --set "${NAME}" drawing=off
+fi
 
 if [ "$1" = "${FOCUSED_WORKSPACE}" ]; then
     sketchybar --set "${NAME}" background.drawing=on
