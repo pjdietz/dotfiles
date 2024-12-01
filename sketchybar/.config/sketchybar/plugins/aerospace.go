e47f05da471775039a5fb89076d44c07a296b0c3
PJ Dietz <pjdietz@gmail.com>
2024-12-01 14:26:23 -0500

    Go sketchybar aerospace shows focus, visible, and apps

--- sketchybar/.config/sketchybar/plugins/aerospace.go
+++ sketchybar/.config/sketchybar/plugins/aerospace.go
@@ -3,12 +3,38 @@ package main
 import (
 	"fmt"
 	"log"
-	"log/slog"
 	"os"
 	"os/exec"
+	"slices"
 	"strconv"
 	"strings"
 )
+
+const (
+	aerospace  = "aerospace"
+	sketchybar = "sketchybar"
+
+	icon_app      = "󰣆"
+	icon_calendar = ""
+	icon_database = ""
+	icon_firefox  = ""
+	icon_finder   = "󰀶"
+	icon_teams    = "󰊻"
+	icon_terminal = ""
+	icon_todo     = ""
+	icon_vpn      = "󰒄"
+)
+
+var icons = map[string]string{
+	"Alacritty":         icon_terminal,
+	"Azure VPN Client":  icon_vpn,
+	"DataGrip":          icon_database,
+	"Finder":            icon_finder,
+	"Firefox":           icon_firefox,
+	"Microsoft Outlook": icon_calendar,
+	"Microsoft Teams":   icon_teams,
+	"Todoist":           icon_todo,
+}
 
 func main() {
 	args := os.Args[1:]
@@ -17,6 +43,8 @@ func main() {
 	}
 
 	switch args[0] {
+	case "status":
+		status()
 	case "init":
 		initialize()
 	case "update":
@@ -28,99 +56,240 @@ func main() {
 	os.Exit(0)
 }
 
+func status() {
+	state := newAerospaceState()
+	fmt.Printf("%+v", state)
+}
+
 func initialize() {
-	fmt.Println("init")
-	monitors, _ := getMonitors()
-	workspaces, _ := getWorkspaces()
+	state := newAerospaceState()
+	binary, _ := os.Executable()
 
-	fmt.Printf("Montiors: %v\n", monitors)
-	fmt.Printf("Workspaces: %v\n", workspaces)
-
-	for i, m := range monitors {
+	// Add a bracket for monitor.
+	for i, m := range state.monitors {
 		args := []string{}
 		bracket := fmt.Sprintf("monitor.%d", m)
 		regex := fmt.Sprintf("/space\\.%d\\..*/", m)
 		spacer := fmt.Sprintf("spacer.%s", bracket)
 
+		// Add spacer between monitors.
 		if i > 0 {
 			args = append(args, "--add", "item", spacer, "center",
-				"--set", spacer,
-				"width=20")
+				"--set", spacer, "width=20")
 		}
 
-		args = append(args, "--add", "bracket", bracket, regex,
-			"--set", bracket)
+		// Add a bracket.
+		args = append(args, "--add", "bracket", bracket, regex, "--set", bracket)
+		_ = exec.Command(sketchybar, args...).Run()
+		args = []string{}
 
-		for _, w := range workspaces {
+		// Add an item for each workspace.
+		for j, w := range state.workspaces {
 			item := fmt.Sprintf("space.%d.%s", m, w)
+			workstate := state.WorkspaceState(m, w)
 
 			args = append(args, "--add", "item", item, "center",
 				"--set", item,
 				"icon="+w,
-				"label.drawing=false",
-				"background.height=24",
-				"background.border_width=1",
-				"background.border_color=0xff999999")
+				"drawing=true",
+				"background.height=30",
+				"click_script=aerospace workspace "+w+"",
+				"script="+binary+" update")
+
+			args = append(args, workstate.Args()...)
+
+			// Only the first item needs to subscribe.
+			if i == 0 && j == 0 {
+				args = append(args, "--subscribe", item, "aerospace_workspace_change")
+			}
 		}
 
-		slog.Info(fmt.Sprintf("%v", args))
-		err := exec.Command("sketchybar", args...).Run()
-		if err != nil {
-			slog.Error(err.Error())
-		}
+		_ = exec.Command(sketchybar, args...).Run()
 	}
 
 }
 
 func update() {
-	fmt.Println("update")
+	state := newAerospaceState()
+	args := []string{}
+	for _, m := range state.monitors {
+		for _, w := range state.workspaces {
+			item := fmt.Sprintf("space.%d.%s", m, w)
+			workstate := state.WorkspaceState(m, w)
+			args = append(args, "--set", item)
+			args = append(args, workstate.Args()...)
+		}
+	}
+	_ = exec.Command(sketchybar, args...).Run()
 }
 
-func getMonitors() ([]int, error) {
-	cmd := exec.Command("aerospace", "list-monitors", "--format", "%{monitor-id}")
-	obytes, err := cmd.Output()
-	if err != nil {
-		return []int{}, err
+// -----------------------------------------------------------------------------
+
+type WorkspaceState struct {
+	focused bool
+	visible bool
+	apps    string
+}
+
+func (w WorkspaceState) Args() []string {
+
+	// Hide any workspace that is empty and not visible.
+	if !w.visible && w.apps == "" {
+		return []string{"drawing=false"}
 	}
-	lines := strings.Split(string(obytes), "\n")
+
+	a := []string{
+		"drawing=true",
+		"label=" + w.apps,
+		"label.drawing=true",
+	}
+
+	if w.focused {
+		a = append(a, "background.color=0x66ffffff")
+	} else if w.visible {
+		a = append(a, "background.color=0x33ffffff")
+	} else {
+		a = append(a, "background.color=0x00000000")
+	}
+
+	return a
+}
+
+type AerospaceState struct {
+	monitors   []int
+	workspaces []string
+	focused    string
+	visible    []string
+	apps       map[int]map[string]string
+}
+
+func (a AerospaceState) WorkspaceState(monitor int, workspace string) WorkspaceState {
+	w := WorkspaceState{}
+
+	appsByWorkspace, ok := a.apps[monitor]
+	if !ok {
+		return w
+	}
+	apps, ok := appsByWorkspace[workspace]
+	if !ok {
+		return w
+	}
+
+	w.focused = workspace == a.focused
+	w.visible = slices.Contains(a.visible, workspace)
+	w.apps = apps
+
+	return w
+}
+
+func newAerospaceState() AerospaceState {
+	return AerospaceState{
+		monitors:   getMonitors(),
+		workspaces: getWorkspaces(),
+		focused:    getFocusedWorkspace(),
+		visible:    getVisibleWorkspaces(),
+		apps:       getApps(),
+	}
+}
+
+func getMonitors() []int {
+	cmd := exec.Command("aerospace", "list-monitors", "--format", "%{monitor-id}")
+	o, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(strings.TrimSpace(string(o)), "\n")
 	monitors := []int{}
 	for _, line := range lines {
-		monitor, err := strconv.Atoi(line)
-		if err == nil {
-			monitors = append(monitors, monitor)
-		}
+		monitor, _ := strconv.Atoi(line)
+		monitors = append(monitors, monitor)
 	}
-	return monitors, nil
+	return monitors
 }
 
-func getWorkspaces() ([]string, error) {
+func getWorkspaces() []string {
 	cmd := exec.Command("aerospace", "list-workspaces", "--all", "--format", "%{workspace}")
-	obytes, err := cmd.Output()
+	o, err := cmd.Output()
 	if err != nil {
-		return []string{}, err
+		log.Fatal(err)
 	}
-	lines := strings.Split(string(obytes), "\n")
+	lines := strings.Split(strings.TrimSpace(string(o)), "\n")
 	workspaces := []string{}
 	for _, line := range lines {
 		if line != "" {
 			workspaces = append(workspaces, line)
 		}
 	}
-	return workspaces, nil
+	return workspaces
 }
 
-func getWindows() ([]string, error) {
-	cmd := exec.Command("aerospace", "list-workspaces", "--all", "--format", "%{workspace}")
-	obytes, err := cmd.Output()
+func getFocusedWorkspace() string {
+	cmd := exec.Command(aerospace, "list-workspaces", "--focused")
+	o, err := cmd.Output()
 	if err != nil {
-		return []string{}, err
+		log.Fatal(err)
 	}
-	lines := strings.Split(string(obytes), "\n")
+	return strings.TrimSpace(string(o))
+}
+
+func getVisibleWorkspaces() []string {
+	cmd := exec.Command(aerospace, "list-workspaces", "--monitor", "all", "--visible")
+	o, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
 	workspaces := []string{}
+	lines := strings.Split(strings.TrimSpace(string(o)), "\n")
 	for _, line := range lines {
 		if line != "" {
 			workspaces = append(workspaces, line)
 		}
 	}
-	return workspaces, nil
+	return workspaces
+}
+
+// Return a map of monitor IDs to map of workspace names to app icons.
+func getApps() map[int]map[string]string {
+	cmd := exec.Command("aerospace", "list-windows", "--monitor", "all",
+		"--format", "%{monitor-id}:%{workspace}:%{app-name}")
+	o, err := cmd.Output()
+	if err != nil {
+		log.Fatalf("Error reading apps. %s", err)
+	}
+
+	// Sorting the lines orders by monitor, workspace, app.
+	lines := strings.Split(strings.TrimSpace(string(o)), "\n")
+	slices.Sort(lines)
+
+	workspacesByMontor := make(map[int]map[string]string)
+
+	for _, l := range lines {
+		tokens := strings.SplitN(l, ":", 3)
+		monitor, _ := strconv.Atoi(tokens[0])
+		workspace := tokens[1]
+		app := tokens[2]
+
+		appsByWorkspace, ok := workspacesByMontor[monitor]
+		if !ok {
+			appsByWorkspace = make(map[string]string)
+			workspacesByMontor[monitor] = appsByWorkspace
+		}
+
+		apps := appsByWorkspace[workspace]
+		icon := appIcon(app)
+		if !strings.Contains(apps, icon) {
+			apps += icon
+		}
+		appsByWorkspace[workspace] = apps
+	}
+
+	return workspacesByMontor
+}
+
+func appIcon(app string) string {
+	icon, ok := icons[app]
+	if !ok {
+		return icon_app
+	}
+	return icon
 }
