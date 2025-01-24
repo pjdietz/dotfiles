29ead0121b866760cfd5c5749e1a5b3f2c141eb0
PJ Dietz <pjdietz@gmail.com>
2025-01-24 13:31:10 -0500

    When cleaning no name buffers, only close if modifiable

--- nvim/.config/nvim/lua/user/buffers.lua
+++ nvim/.config/nvim/lua/user/buffers.lua
@@ -6,8 +6,9 @@ function M.delete_noname_buffers()
   for _, bufno in ipairs(buflist) do
     local noname = vim.api.nvim_buf_get_name(bufno) == ""
     local listed = vim.bo[bufno].buflisted
+    local modifiable = vim.api.nvim_get_option_value("modifiable", { buf = bufno })
     local modified = vim.api.nvim_get_option_value("modified", { buf = bufno })
-    if noname and listed and bufno ~= curbufno and not modified then
+    if noname and listed and bufno ~= curbufno and modifiable and not modified then
       vim.cmd("bd " .. tostring(bufno))
     end
   end
