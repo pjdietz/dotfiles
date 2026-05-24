129db0c54debcb03f77632c4721a2af39c32ade3
PJ Dietz <pjdietz@gmail.com>
2026-05-24 09:10:25 -0400

    Convert hyprland config to Lua

---
+++ hypr/.config/hypr/colors.lua
@@ -0,0 +1,63 @@
+-- Catpuccin mocha
+
+local rosewater = 0xfff5e0dc
+local flamingo  = 0xfff2cdcd
+local pink      = 0xfff5c2e7
+local mauve     = 0xffcba6f7
+local red       = 0xfff38ba8
+local maroon    = 0xffeba0ac
+local peach     = 0xfffab387
+local yellow    = 0xfff9e2af
+local green     = 0xffa6e3a1
+local teal      = 0xff94e2d5
+local sky       = 0xff89dceb
+local sapphire  = 0xff74c7ec
+local blue      = 0xff89b4fa
+local lavender  = 0xffb4befe
+
+local text      = 0xffcdd6f4
+local subtext1  = 0xffbac2de
+local subtext0  = 0xffa6adc8
+local overlay2  = 0xff9399b2
+local overlay1  = 0xff7f849c
+local overlay0  = 0xff6c7086
+local surface2  = 0xff585b70
+local surface1  = 0xff45475a
+local surface0  = 0xff313244
+local base      = 0xff1e1e2e
+local mantle    = 0xff181825
+local crust     = 0xff11111b
+
+local shadow          = 0x99000000
+local translucent_base = 0xcc1e1e2e
+
+return {
+    rosewater = rosewater,
+    flamingo  = flamingo,
+    pink      = pink,
+    mauve     = mauve,
+    red       = red,
+    maroon    = maroon,
+    peach     = peach,
+    yellow    = yellow,
+    green     = green,
+    teal      = teal,
+    sky       = sky,
+    sapphire  = sapphire,
+    blue      = blue,
+    lavender  = lavender,
+    text      = text,
+    subtext1  = subtext1,
+    subtext0  = subtext0,
+    overlay2  = overlay2,
+    overlay1  = overlay1,
+    overlay0  = overlay0,
+    surface2  = surface2,
+    surface1  = surface1,
+    surface0  = surface0,
+    base      = base,
+    mantle    = mantle,
+    crust     = crust,
+    shadow          = shadow,
+    translucent_base = translucent_base,
+}
