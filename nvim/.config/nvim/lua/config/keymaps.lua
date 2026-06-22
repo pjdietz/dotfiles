060a980e463d5da314b859d2beb654584ea8613f
PJ Dietz <pjdietz@gmail.com>
2026-06-22 13:20:02 -0400

    Add nvim keybind to toggle spell

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -67,6 +67,11 @@ vim.keymap.set("n", "<leader>tw", function()
   vim.wo.wrap = not vim.wo.wrap
 end, { desc = "Toggle line [w]rap" })
 
+-- Toggle spelling
+vim.keymap.set("n", "<leader>tz", function()
+  vim.wo.spell = not vim.wo.spell
+end, { desc = "Toggle spellng" })
+
 -- Git and merge
 map("n", "<Leader>gh", "<CMD>diffget //2<CR>", { desc = "[G]it merge take left" })
 map("n", "<Leader>gl", "<CMD>diffget //3<CR>", { desc = "[G]it merge take right" })
